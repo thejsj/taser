04b32857bac6c4de3db3c394506f29bc46ee5f2f
thejsj <jorge.silva@thejsj.com>
2015-06-05 19:00:32 -0700

    Adding value tests

--- test/index.js
+++ test/index.js
@@ -1,5 +1,6 @@
 "use strict";
 let taser = require('../taser');
+require('should');
 
 describe('Taser', () => {
 
@@ -14,13 +15,52 @@ describe('Taser', () => {
       taser([1, 2, 3], ['array']);
     });
 
-    it('should recognize an array as an object', () => {
-      taser.bind(null, [1, 2, 3], ['object']).should.throw();
+    it('should not recognize an array as an object', () => {
+      taser.bind(null, [1, 2, 3], ['object']).should.throw;
+    });
+
+    it('should not recognize `null` as an object', () => {
+      taser.bind(null, null, ['object']).should.throw;
+    });
+
+    describe('Passing Objects', () => {
+
+      it('should takes objects with a `type` property for the second argument', () => {
+        taser('hello', {
+          types: ['string']
+        });
+        taser(3, {
+          types: ['number']
+        });
+        taser(null, {
+          types: ['null']
+        });
+        taser(undefined, {
+          types: ['undefined']
+        });
+        taser({ a: 1 }, {
+          types: ['object']
+        });
+      });
     });
 
   });
 
   describe('Value Checking', function () {
+
+    it('should take `values` property with an array of valid values', () => {
+      taser('hello', {
+        types: ['string'],
+        values: 'hello'
+      });
+    });
+
+    it('should take `values` property with an array of valid values', () => {
+      taser.bind(null, 'hello', {
+        types: ['string'],
+        values: 'not-hello'
+      }).should.throw;
+    });
 
   });
 });
